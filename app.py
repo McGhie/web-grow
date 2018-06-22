f11db482e768d925605214553be4db82e3ea57c2
McGhie <argyllmcghie@gmail.com>
2018-06-22 21:05:33 +1000

    update 20a

--- app.py
+++ app.py
@@ -7,31 +7,12 @@ Complete project details: http://randomnerdtutorials.com
 
 '''
 
-import RPi.GPIO as GPIO
+
 from flask import Flask, render_template, request
+import pinboard
 app = Flask(__name__)
 
-GPIO.setmode(GPIO.BOARD)
-on = 'on'
-
-# Create a dictionary called pins to store the pin number, type, and pin state:
-pins = {
-   1 : {'type' : '3.3v', 'state' : on},
-   2 : {'type' : '5v', 'state' : on},
-   3 : {'type' : 'GPIO 02', 'state' : GPIO.LOW},
-   4 : {'type' : '5v', 'state' : on},
-   5 : {'type' : 'GPIO 03', 'state' : GPIO.LOW},
-   6 : {'type' : 'Ground', 'state' : GPIO.LOW},
-   7 : {'type' : 'GPIO 04', 'state' : GPIO.LOW},
-   8 : {'type' : 'GPIO 14', 'state' : GPIO.LOW},
-   9 : {'type' : 'Ground', 'state' : GPIO.LOW},
-   10 : {'type' : 'GPIO 15', 'state' : GPIO.LOW},
-   11 : {'type' : 'GPIO 17', 'state' : GPIO.LOW},
-   12 : {'type' : 'GPIO 18', 'state' : GPIO.LOW},
-   13 : {'type' : 'GPIO 27', 'state' : GPIO.LOW},
-   14 : {'type' : 'Ground', 'state' : GPIO.LOW},
-
-   }
+pins = pinboard.pins
 
 # Set each pin as an output and make it low:
 for pin in pins:
