777139d56b4acb22c9dd1be9c7dfd47256d46b0e
a <argyllmcghie@gmail.com>
2018-10-27 05:40:55 +1100

    dm.py

---
+++ py/dm.py
@@ -0,0 +1,15 @@
+from time import sleep
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
+#GPIO.output(12, GPIO.HIGH)
+
+p = GPIO.PWM(12, 50)
+
+p.start(100)
+
+sleep(5)
+p.stop()        
+
+GPIO.cleanup()
