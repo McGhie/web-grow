ea4495aaf5073fa810b14b76f0a154fb60e47efe
a <argyllmcghie@gmail.com>
2018-10-27 06:43:27 +1100

    dsasdff

--- py/dm.py
+++ py/dm.py
@@ -4,10 +4,13 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
 GPIO.setup(16, GPIO.OUT)
-GPIO.output(16, GPIO.HIGH)
 GPIO.setup(18, GPIO.OUT)
+
+GPIO.output(16, GPIO.LOW)
 GPIO.output(18, GPIO.HIGH)
 
+sleep(1)
+GPIO.output(16, GPIO.HIGH)
 p = GPIO.PWM(12, 50)
 
 p.start(100)
@@ -15,8 +18,7 @@ p.start(100)
 sleep(5)
 p.ChangeDutyCycle(0)
 GPIO.output(16, GPIO.LOW)
-GPIO.output(12, GPIO.LOW)
-sleep(5)
+GPIO.output(18, GPIO.LOW)
 p.stop()
 
 GPIO.cleanup()
