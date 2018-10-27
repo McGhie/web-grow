16ae590344f76a7865feaac3533c94034a72e1f8
a <argyllmcghie@gmail.com>
2018-10-27 14:45:33 +1100

    dsasdffad

--- py/dm.py
+++ py/dm.py
@@ -14,18 +14,11 @@ GPIO.output(16, GPIO.LOW)
 GPIO.output(18, GPIO.HIGH)
 sleep(1)
 
-GPIO.output(16, GPIO.HIGH)
+
 p = GPIO.PWM(12, 50)
 
 p.start(100)
 sleep(1)
-GPIO.output(16, GPIO.LOW)
-
-
-sleep(2)
-p.ChangeDutyCycle(0)
-GPIO.output(16, GPIO.LOW)
 GPIO.output(18, GPIO.LOW)
 p.stop()
-
 GPIO.cleanup()
