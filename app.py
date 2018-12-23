702499449ab1cca07dd46c4b028240b9a794f5c8
Argyll <argyllmcghie@gmail.com>
2018-12-23 12:00:20 +1100

    fdsg
    sdg

--- app.py
+++ app.py
@@ -92,33 +92,7 @@ pump_thread_event = Event()
 print ("Pump thread is: " + str(pump_thread_event.isSet()))
 
 
-
-# class RandomThread(Thread):
-#     def __init__(self):
-#         self.delay = 10
-#         super(RandomThread, self).__init__()
-#
-#     def randomNumberGenerator(self):
-#         """
-#         Generate a random number every 1 second and emit to a socketio instance (broadcast)
-#         Ideally to be run in a separate thread?
-#         """
-#         #infinite loop of magical random numbers
-#         print("Making random numbers")
-#         while not thread_stop_event.isSet():
-#             number = round(random()*10, 3)
-#             print(number)
-#             socketio.emit('newnumber', {'number': number}, namespace='/test')
-#             sleep(self.delay)
-#
-#     def run(self):
-#         self.randomNumberGenerator()
-
-
-#def run(self):
-#    self.hardcoded_10_min_pump_on()
-#/////////////
-
+\
 
 ##@app.route("/index")
 def main():
